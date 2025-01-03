d42485effd08beb6231333d596f223dda5c885fe
Henry Ward <45144290+kudosscience@users.noreply.github.com>
2025-01-03 11:44:36 +0000

    Add Hello World and Hello Express

--- myApp.js
+++ myApp.js
@@ -1,6 +1,14 @@
 let express = require('express');
 let app = express();
 
+console.log("Hello World");
+
+// Use the app.get() method to serve the string "Hello Express" to GET requests matching the / (root) path.
+app.get('/', (req, res) => {
+  res.send('Hello Express');
+}
+);
+
 
 
 

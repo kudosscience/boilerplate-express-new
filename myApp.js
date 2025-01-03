366367e0a5d49defec5bdbe9168252a515fa9675
Henry Ward <45144290+kudosscience@users.noreply.github.com>
2025-01-03 12:08:18 +0000

    Add express middleware for CSS

--- myApp.js
+++ myApp.js
@@ -4,14 +4,19 @@ let app = express();
 console.log("Hello World");
 
 // Use the app.get() method to serve the string "Hello Express" to GET requests matching the / (root) path.
+// app.get('/', (req, res) => {
+//   res.send('Hello Express');
+// }
+// );
+
+// Send the /views/index.html file as a response to GET requests to the / path.
 app.get('/', (req, res) => {
-  res.send('Hello Express');
+  res.sendFile(__dirname + '/views/index.html');
 }
 );
 
-
-
-
+// Mount the express.static() middleware to the path /public with app.use(). The absolute path to the assets folder is __dirname + /public
+app.use('/public', express.static(__dirname + '/public'));
 
 
 
